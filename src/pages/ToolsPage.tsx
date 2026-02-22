85a252ef42dbfb5e80786468a1b354bf37b52435
Anya Cunha <anyacontato@gmail.com>
2026-02-22 10:36:15 -0300

    atualizações tracker

--- src/pages/ToolsPage.tsx
+++ src/pages/ToolsPage.tsx
@@ -1,7 +1,13 @@
+import { analytics } from "@/utils/analyticsUtils";
+import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 
 export const ToolsPage = () => {
+  useEffect(() => {
+    analytics.track("cadsig_tools_page_view");
+  }, []);
+
   return (
     <Container fluid className="p-0">
       <div style={{ minHeight: "calc(100vh - 192px)" }}>
